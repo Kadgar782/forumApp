f7e62527eefbcecdcdb269313864ac8a4ac54600
Kadgar782 <bogdan7823@mail.ru>
2023-08-23 16:54:53 +0600

    Hide ReactQueryDevtools in production

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,15 +10,16 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient({
   // the time in the fresh state is two minutes, but it is possible that it is better for the user to always have everything in the stale state,
   //  so he will not be able to go to an already deleted post or see some posts later than others
-  defaultOptions: { queries: { staleTime: 1000 * 60 * 2} },
+  defaultOptions: { queries: { staleTime: 1000 * 60 * 2 } },
 });
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const isProduction = process.env.NODE_ENV
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
-        <ReactQueryDevtools />
+        { isProduction === "development" ? <ReactQueryDevtools /> : null}
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
